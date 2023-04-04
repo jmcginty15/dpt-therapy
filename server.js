f3f1d1b97684d9d2593741810b3d238f4ec55cb9
Jason McGinty <jasonmcginty@Jasons-MacBook-Air.local>
2023-04-04 15:37:34 -0500

    update client routes

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 
-const CLIENT_ROUTES = ['/', '/about', '/get-started', '/services', '/patient-experiences', '/location', '/nondiscrimination-disclosure-statement'];
+const CLIENT_ROUTES = [
+  "/",
+  "/about",
+  "/get-started",
+  "/services",
+  "/patient-experiences",
+  "/locations",
+  "/nondiscrimination-disclosure-statement",
+];
 app.use((req, res, next) => {
-    if (CLIENT_ROUTES.includes(req.path)) {
-        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-        res.header('Expires', '-1');
-        res.header('Pragma', 'no-cache');
-        res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-    } else {
-        next();
-    }
+  if (CLIENT_ROUTES.includes(req.path)) {
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    res.header("Expires", "-1");
+    res.header("Pragma", "no-cache");
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  } else {
+    next();
+  }
 });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
